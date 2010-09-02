2b49cb0c5fc88806ed7010d88f5de3b9ac426402
Your Name <Leo-sp50@yandex.ru>
2010-09-02 22:02:18 +0400

    merge master antique

--- globals.mk
+++ globals.mk
@@ -8,7 +8,7 @@ INFO_ARCH=i586
 TARGET=i586
 INFO_LABEL=Antique
 
-INFO_VERSION=5.0
+INFO_VERSION=5.1
 
 # FIXME: INFO_* won't fall through to profiles/*/Makefile
 GLOBAL_RELEASE=$(INFO_RELEASE)
@@ -16,7 +16,7 @@ GLOBAL_RELEASE=$(INFO_RELEASE)
 GLOBAL_BOOT_LANG=ru_RU
 GLOBAL_HSH_APT_CONFIG=/etc/apt/apt.conf
 
-GLOBAL_TOPDIR=/home/inter/mkiso/boyrsh/mkimage-profiles-desktop
+GLOBAL_TOPDIR=/home/inter/mkiso/github/antique
 GLOBAL_WORKROOT=$(TMP)
 
 DATE=$(shell date +%Y%m%d)
